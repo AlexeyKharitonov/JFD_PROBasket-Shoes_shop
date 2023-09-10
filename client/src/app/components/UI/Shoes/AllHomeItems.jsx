9fc0cf002e1470e6cbd6ab490c7dc5e9fb226c89
Alexey Kharitonov <alexeywest024@list.ru>
2023-09-10 22:39:12 +0300

    fix bug with pagination

--- client/src/app/components/UI/Shoes/AllHomeItems.jsx
+++ client/src/app/components/UI/Shoes/AllHomeItems.jsx
@@ -93,7 +93,7 @@ const AllHomeItems = () => {
     category &&
     !loadingCategoriesLoadingStatus
   ) {
-    const count = product.length;
+    const count = displayItems.length;
     const paginate = (items, pageNumber, pageSize) => {
       const startIndex = (pageNumber - 1) * pageSize;
       return [...items].slice(startIndex, startIndex + pageSize);
